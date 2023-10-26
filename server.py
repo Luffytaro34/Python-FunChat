cbff25bb09af0b350a15e828879d17e54f43e5ca
Luffytaro34 <140912669+Luffytaro34@users.noreply.github.com>
2023-10-26 20:52:09 +0530

    Create server.py
    This is a server used for the application which is coded using sockets module in python

---
+++ server.py
@@ -0,0 +1,86 @@
+# Import required modules
+import socket
+import threading
+
+host = '127.0.0.1'
+port = 1234 # You can use any port between 0 to 65535
+listener_lim = 5
+online_clients = [] # List of all currently connected users
+
+# Function to listen for upcoming messages from a client
+def listen_for_messages(client, username):
+
+    while True:
+
+        message = client.recv(2048).decode('utf-8')
+        if message != '':
+            
+            final_msg = username + '~' + message
+            send_messages_to_all(final_msg)
+
+        else:
+            print(f"The message send from client {username} is empty")
+
+
+# Function to send message to a single client
+def send_message_to_client(client, message):
+
+    client.sendall(message.encode())
+
+# Function to send any new message to all the clients that
+# are currently connected to this server
+def send_messages_to_all(message):
+    
+    for user in online_clients:
+
+        send_message_to_client(user[1], message)
+
+# Function to handle client
+def client_handler(client):
+    
+    # Server will listen for client message that will
+    # Contain the username
+    while True:
+
+        username = client.recv(2048).decode('utf-8')
+        if username != '':
+            active_clients.append((username, client))
+            prompt_message = "SERVER~" + f"{username} added to the chat"
+            send_messages_to_all(prompt_message)
+            break
+        else:
+            print("Client username is empty")
+
+    threading.Thread(target=listen_for_messages, args=(client, username, )).start()
+
+# Main function
+def main():
+
+    # Creating the socket class object
+    # AF_INET: we are going to use IPv4 addresses
+    # SOCK_STREAM: we are using TCP packets for communication
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Creating a try catch block
+    try:
+        # Provide the server with an address in the form of
+        # host IP and port
+        server.bind((host, port))
+        print(f"Running the server on {host} {port}")
+    except:
+        print(f"Unable to bind to host {host} and port {port}")
+
+    # Set server limit
+    server.listen(listener_lim)
+
+    # This while loop will keep listening to client connections
+    while True:
+
+        client, address = server.accept()
+        print(f"Successfully connected to client {address[0]} {address[1]}")
+
+        threading.Thread(target=client_handler, args=(client, )).start()
+
+
+if __name__ == '__main__':
+    main()
